5c5c856c30ca79feff06d71222092d7580894dda
BYEDUCK <mateuszbajdak@gmail.com>
2020-04-04 15:01:23 +0200

    added schwefel function

--- utils/functions.py
+++ utils/functions.py
@@ -6,7 +6,10 @@ def griewank(x):
     part_2 = reduce(lambda a, b : a * b, map(lambda a : math.cos(a[1] / math.sqrt(a[0] + 1)), enumerate(x)))
     return 1 + part_1 / 4000 - part_2
 
+def schwefel(x):
+    return 418.9829 * len(x)  - sum(map(lambda a : a * math.sin(math.sqrt(abs(a))), x))
 
 if __name__ == "__main__":
     x = [2, 2, 3]
     print(griewank(x))
+    print(schwefel(x))
